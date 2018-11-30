5f04e8ce8390b2ac45880bda10ed279c823400f0
Riccardo Straccia <merutochan@gmail.com>
2018-11-30 20:29:37 +0100

    Add files via upload
    Added main files to the repository.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+gcc main.c -lm -o main
+echo "Generating pictures..."
+parallel ./main {} "$1" :::: <(seq 0 "$1")
+mkdir $HOME/Frames &> /dev/null;
+cd $HOME/Frames;
+echo "Creating video..."
+ffmpeg -i %01d.ppm -framerate 60 -pattern_type sequence -y out.mp4 &> /dev/null
+rm *.ppm >& /dev/null
+echo "Creating palette..."
+ffmpeg -i out.mp4 -vf "fps=30,scale=160:-1:flags=lanczos,palettegen=stats_mode=diff" -y 'palette.png' >& /dev/null
+echo "Creating gif..."
+ffmpeg -i out.mp4 -i palette.png -lavfi "fps=30,scale=160:-1:flags=lanczos,paletteuse=dither=bayer:bayer_scale=2:diff_mode=rectangle" -y out.gif &> /dev/null
+rm palette.png
